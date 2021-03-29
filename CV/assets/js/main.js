89b5bbe22c640ea437377f71b1988b7fe14223f4
Adrien88 <adrien.boilley@gmail.com>
2021-03-29 11:24:52 +0200

    add js modules

---
+++ CV/assets/js/main.js
@@ -0,0 +1,5 @@
+import { summaryMenu } from "./summaryMenu.js";
+summaryMenu();
+
+import { popup } from "./popup.js";
+window.popup = popup;
---
+++ CV/assets/js/popup.js
@@ -0,0 +1,33 @@
+export function popup(id)
+{
+    let popup = document.createElement('div');
+    popup.classList.add('popup-background');
+
+    let popupContent = document.createElement('div');
+    popupContent.classList.add('popup-container');
+
+    let elem = document.getElementById(id);
+    if (elem !== null){
+        elem = elem.cloneNode(true);
+        elem.classList.remove('hidden');
+        popupContent.appendChild(elem);
+    }
+
+    window.addEventListener('keydown', (e)=>{
+        if (e.key == 'Escape')
+        document.getElementById('body').removeChild(popup);
+    });
+
+    let closeButton = document.createElement('button');
+    closeButton.addEventListener('click', ()=>{
+        document.getElementById('body').removeChild(popup);
+    });
+    closeButton.innerHTML = 'Close windows'
+
+    let hr = document.createElement('hr'); 
+    popupContent.appendChild(hr);
+    popupContent.appendChild(closeButton);
+    popup.appendChild(popupContent);
+    document.getElementById('body').appendChild(popup);
+}
+
