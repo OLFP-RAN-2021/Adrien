0bb954e7c9c16ffcf49c398b5807ec7b8811e977
Adrien88 <adrien.boilley@gmail.com>
2021-03-30 16:06:40 +0200

    add import

--- CV/assets/js/main.js
+++ CV/assets/js/main.js
@@ -1,5 +1,12 @@
+
 import { summaryMenu } from "./summaryMenu.js";
-summaryMenu();
+summaryMenu('menu-area', 'menu-item');
 
 import { popup } from "./popup.js";
 window.popup = popup;
+
+import { formHandler } from "./formHandler.js";
+formHandler();
+
+
+
