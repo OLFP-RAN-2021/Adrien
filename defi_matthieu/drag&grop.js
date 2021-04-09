2d4ec9aeee8818928b0c62cd32d89d3cf06ba460
Adrien88 <adrien.boilley@gmail.com>
2021-04-09 19:28:15 +0200

    finish ou presque

--- defi_matthieu/drag&grop.js
+++ defi_matthieu/drag&grop.js
@@ -18,21 +18,78 @@ function dropHandler(event)
         .then((image)=>{
             console.log(image);
             
-            let imgurl = URL.createObjectURL(image);
-            // console.log(imgurl);    
-        
-            // Afficher le résultat
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+
+
+            let resizer = document.getElementById('resize');
+            let tw = parseInt(resizer.value) ?? 200;
+            if (tw > 10000) {
+                tw = 10000;
+                resizer.value = tw;
+            }
+
+            // Hauteur en conservant le ratio
+            let th = Math.floor(tw * (image.height/image.width));
+
+            console.log('tw = '+tw+'px; th = '+th+'px;')
+            // console.log((image.height/image.width))
+            // console.log((th/tw))
+
+            canvas.width = tw;
+            canvas.height = th;
+
+            let turn = parseInt(document.getElementById('rotation').value);
+            switch (turn) 
+            {
+                case 90:
+                    canvas.width = th;
+                    canvas.height = tw;
+
+                    ctx.rotate((90 * Math.PI / 180));
+                    ctx.translate(0, (th*-1));
+                break;
+
+                case 180:
+                    ctx.rotate((180 * Math.PI / 180));
+                    ctx.translate((tw*-1), (th*-1));
+                break;
+
+                case 230:
+                    console.log('rotate 230')
+                    canvas.width = th;
+                    canvas.height = tw;
+
+                    ctx.rotate((-90 * Math.PI / 180));
+                    ctx.translate((tw*-1), 0);
+                break;
+
+                default:
+                break;
+            }
+
+            /**
+             * @param img
+             * @param int algin in x in source img
+             * @param int algin in y in source img
+             * @param int get x px in lenght of source img
+             * @param int get y px in height of source img
+             * @param int algin in x in canvas
+             * @param int algin in y in canvas
+             * @param int canvas x px width in piXels
+             * @param int canvas Y px height in piXels
+             */
+            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, tw, th);
+            // ctx.rotate((90 * Math.PI / 180));
+
+            // console.log(ctx);
             let target = document.getElementById('printResult');
-            let img = document.createElement('img');
-            
-        
-            img.setAttribute('src', imgurl);
-            target.appendChild(img);
+            target.appendChild(canvas);
 
         });
         // console.log(htmlImageElement);
 
-        // Créer une url pour le fichier
+        // // Créer une url pour le fichier
         // let imgurl = URL.createObjectURL(file);
         // // console.log(imgurl);    
     
@@ -62,3 +119,8 @@ function dragOverHandler(ev) {
     ev.preventDefault();
   }
   
+
+  function clean()
+  {
+      document.getElementById('printResult').innerHTML = '';
+  }
