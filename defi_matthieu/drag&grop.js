afc1ebf4e1a0537aa51c3b481adbf2c39dec137d
Adrien88 <adrien.boilley@gmail.com>
2021-04-09 11:46:47 +0200

    *

--- defi_matthieu/drag&grop.js
+++ defi_matthieu/drag&grop.js
@@ -1,4 +1,8 @@
 
+/**
+ * 
+ * @param {*} event 
+ */
 function dropHandler(event) 
 {
     event.preventDefault();
@@ -6,10 +10,48 @@ function dropHandler(event)
     if (event.dataTransfer.items)
     for(const item of event.dataTransfer.items) {
         
+        // récupère chaque objet comme un fichier
+        let file = item.getAsFile();
+        // console.log(file.stream());
 
-        console.log(item);
+        createImageBitmap(file)
+        .then((image)=>{
+            console.log(image);
+            
+            let imgurl = URL.createObjectURL(image);
+            // console.log(imgurl);    
+        
+            // Afficher le résultat
+            let target = document.getElementById('printResult');
+            let img = document.createElement('img');
+            
+        
+            img.setAttribute('src', imgurl);
+            target.appendChild(img);
+
+        });
+        // console.log(htmlImageElement);
+
+        // Créer une url pour le fichier
+        // let imgurl = URL.createObjectURL(file);
+        // // console.log(imgurl);    
+    
+        // // Afficher le résultat
+        // let target = document.getElementById('printResult');
+        // let img = document.createElement('img');
+        
+        // img.setAttribute('width', '200');
+        // img.setAttribute('height', '150');
+
+        // img.setAttribute('src', imgurl);
+        // target.appendChild(img);
+
     }
 }
+
+
+
+
 
 /**
  * Kill default action of browser
