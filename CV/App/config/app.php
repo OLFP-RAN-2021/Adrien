38de54cca9582c4007e2d0e485ec67fa85fde1e0
= <=>
2021-05-10 11:57:12 +0200

    db config change and add .htaccess to securize a bit.

--- CV/App/config/app.php
+++ CV/App/config/app.php
@@ -1,19 +1,58 @@
 <?php
 return [
+    // 
     "name" => "MyApp",
-    "description" => "Ma suoer App !",
+
+    // app description
+    "description" => "Ma super App !",
+
+    // app owner
     "owner" => "Boilley Adrien",
+
+    // licence
     "licence" => "MIT Licence",
+
+    // namespace
     "namespace" => "App",
+
+    // autoloader folder
     "autoloader" => "App/src/includes",
+
     "router" => [
         "controllers" => "App\\controllers\\",
         "default" => [
             "call" => "App\\controllers\\Page",
             "args" => ["home.html"]
         ],
-        "behaviur" => ""
+        "behavior" => ""
     ],
+
+    "PDO_connect" => [
+
+        'MyAppDB' => [
+
+            // db name
+            'myApp',
+
+            // hostname 
+            'localhost',
+
+            // login
+            'dev',
+
+            // password
+            'dev',
+
+            // db type
+            'mysql',
+
+            // charset
+            'utf8mb4-general-ci',
+
+            // collation
+            'utf8mb4-general-ci',
+        ],
+    ]
 
 
 ];
