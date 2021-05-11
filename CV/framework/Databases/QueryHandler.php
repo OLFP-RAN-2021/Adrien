d0a049fb5322d6b30694c4632d7d701f82d4d82c
= <=>
2021-05-11 16:04:39 +0200

    fixes

--- CV/framework/Databases/QueryHandler.php
+++ CV/framework/Databases/QueryHandler.php
@@ -4,6 +4,7 @@ namespace Framework\Databases;
 
 use Framework\Exception;
 use PDO;
+use PDOException;
 use PDOStatement;
 
 class QueryHandler
@@ -12,7 +13,7 @@ class QueryHandler
     private PDO $PDO;
 
     /**
-     * Construct  
+     * Construct 
      */
     function __construct(string $dbname)
     {
@@ -27,16 +28,13 @@ class QueryHandler
      */
     function prepare(string $request, array $optionsDriver = [])
     {
-        $this->request = $request;
         $this->statement = $this->PDO->prepare($request, $optionsDriver);
         return $this;
     }
 
     /**
-     * 
      *  Chercher à éviter les conflits
      *  UPDATE foo SET (:id = id) WHERE id = id ;
-     * 
      */
     function bind(array $data): self
     {
@@ -44,42 +42,62 @@ class QueryHandler
         if (null === $this->statement) {
             throw new Exception(['message' => "QueryHandler require to prepare statement before binding data."]);
         } else {
-            $this->dataKeys = [];
             foreach ($data as $key => $value) {
-                $okey = $key;
-                while (in_array($key, $this->dataKeys)) {
-                    $key = '_' . $key;
-                }
-                $this->dataKeys[] = $key;
-
-                $this->request = str_replace($okey, $key, $this->request);
                 $this->statement->bindValue($key, $value);
             }
-
-            if (!empty($this->statement->errorInfo())) {
-                throw new Exception(['message' => $this->statement->errorInfo()]);
-            }
+            $this->catchErrors();
         }
-
         return $this;
     }
 
     /**
+     * catchErrors()
      * 
+     * @param void
+     * @return void
+     * @throw Exception if error dropped by PDO or mysql.
      */
-    function execute(array $data)
+    function catchErrors()
     {
-        if (null === $this->statement) {
-            throw new Exception(['message' => "QueryHandler require to prepare statement before execution."]);
+        $error = $this->statement->errorInfo();
+        if (!empty($error[0]) && $error[0] != 00000) {
+            throw new Exception(['message' => $error[0] . '' . $this->reqest]);
         }
-        $this->statement->execute();
     }
 
     /**
      * 
      */
+    function execute()
+    {
+        if (null === $this->statement) {
+            throw new Exception(['message' => "QueryHandler require to prepare statement before execution."]);
+        }
+        try {
+            $this->statement->execute();
+        } catch (PDOException $error) {
+            throw new Exception([
+                'message' => $error->getMessage(),
+                "code" => 303,
+                "throwable" => $error
+            ]);
+        }
+        $this->catchErrors();
+        return $this;
+    }
+
+    /**
+     * Fetch.
+     * 
+     * 
+     */
     function fetch(...$fetchMethod)
     {
-        return $this->statement->fetchAll(...$fetchMethod);
+        if (null === $this->statement) {
+            throw new Exception(['message' => "QueryHandler require to execute statement before fetching."]);
+        }
+        $array = $this->statement->fetchAll(...$fetchMethod);
+        $this->catchErrors();
+        return $array;
     }
 }
