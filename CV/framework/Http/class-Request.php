b28781b3c9952ba92c2e9427bd37cdf0cfa72aaa
= <=>
2021-05-18 16:02:13 +0200

    with objects

--- CV/framework/Http/class-Request.php
+++ CV/framework/Http/class-Request.php
@@ -1,21 +1,32 @@
-<?php 
+<?php
+
 namespace app\http;
 
-class Request {
+use stdClass;
+
+class Request
+{
 
     public function __construct()
     {
-        if (isset($_POST) && !empty($_POST))
-        {
-            $this->POST = $_POST;
+        if (isset($_POST) && !empty($_POST)) {
+            $this->POST = new stdClass;
+            foreach ($_POST as $key => $val)
+                $this->POST->$key = $val;
         }
 
-        if (isset($_GET) && !empty($_GET))
-        {
-            $this->POST = $_POST;
+        if (isset($_GET) && !empty($_GET)) {
+            $this->GET = new stdClass;
+            $this->GET->pathinfo = PATHINFO;
+            foreach ($_GET as $key => $val)
+                $this->GET->$key = $val;
         }
     }
 
-
- 
+    /**
+     * 
+     */
+    public function addHook()
+    {
+    }
 }
