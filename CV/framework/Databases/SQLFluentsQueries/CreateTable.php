9f13bf4ae3e72e5bf77636e919d06035b073dc20
= <=>
2021-05-20 11:40:43 +0200

    add create table

---
+++ CV/framework/Databases/SQLFluentsQueries/CreateTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Framework\Databases\SQLFluentsQueries;
+
+use Framework\Databases\Query;
+
+abstract class CreateTable extends AbstractCmd
+{
+
+    function __constuct(Query $parent)
+    {
+        $this->parent = $parent;
+        $this->request = 'CREATE TABLE IF NOT EXISTS ';
+    }
+
+    function callback(string $tablename = '', ?array $describ = [], ?string $charset = 'utf8mb4', ?string $collation = 'utf8mb4_general_ci')
+    {
+        $this->request .= $tablename . '';
+        if (isset($describ) && !empty($describ)) {
+            $this->request .= '(';
+            foreach ($describ as $row) {
+                $this->request .= $row;
+            }
+            $this->request .= ')';
+        }
+        if ($charset) {
+            $this->request .= ' CHARACTER SET ' . $charset;
+        }
+        if ($collation) {
+            $this->request .= ' COLLATE ' . $collation;
+        }
+    }
+}
