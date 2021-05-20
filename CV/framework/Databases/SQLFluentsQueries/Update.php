82e6b300d89c11ba33a4eb397ca0a62c1890358c
= <=>
2021-05-20 10:58:04 +0200

    fix

--- CV/framework/Databases/SQLFluentsQueries/Update.php
+++ CV/framework/Databases/SQLFluentsQueries/Update.php
@@ -6,28 +6,44 @@ use Framework\Databases\Query;
 
 class Update extends AbstractCmd
 {
-    static $selcall = 0;
+    static $ccalled = 0;
 
-    function __construct(Query $parent, array $data)
+    function __construct(Query $parent)
     {
-        ++self::$selcall;
-    }
-
-
-    function callback()
-    {
-    }
-
-    function __toString(): string
-    {
-        return 'UPDATE :' . implode(',:', array_keys($this->args)) . ' ';
+        $this->objcalled = 0;
+        ++self::$ccalled;
+        $this->request = ' UPDATE ';
     }
 
     /**
      * 
      */
-    function solve(): array
+    function callback(string $tablename = '', array $data = [])
     {
-        return [];
+        if (!empty($tablename)) {
+            $this->request .= $tablename . ' SET  ';
+        }
+        ++$this->objcalled;
+        $i = 0;
+
+        // foreach ($data as $row) {
+        //     $this->request .= '(';
+        foreach ($data as $key => $value) {
+            $i++;
+            $nkey = self::$ccalled . $this->objcalled . 'update' . $i . $key;
+            $this->data[$nkey] = $value;
+            $this->request .= $key . ' = :' . $nkey . ', ';
+        }
+        $this->request = substr($this->request, 0, -1);
+        //     $this->request .= '),';
+        // }
+    }
+
+    /**
+     * 
+     */
+    function solve()
+    {
+        $this->request = substr($this->request, 0, -1) . ' ';
     }
 }
