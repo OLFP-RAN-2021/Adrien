ebd0fd89e3457f51bbb8a19f831353b100eff08b
= <=>
2021-05-21 10:05:08 +0200

    rename file & fixe

--- CV/framework/Databases/SQLElements/keys/UniqueKey.php
+++ CV/framework/Databases/SQLElements/keys/UniqueKey.php
@@ -2,13 +2,16 @@
 
 namespace Framework\Databases\SQLElements\Key;
 
+use Framework\Databases\SQLElements\Type;
+
 class UniqueKey extends Key
 {
     function __construct(
         string $name,
+        private Type $type,
         ?string $index = null,
 
     ) {
-        parent::__construct($name, $index, true);
+        parent::__construct($name, $type, $index, true);
     }
 }
